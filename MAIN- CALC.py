c42edb1fde3159a6dab24cf9f01aa31d3f982eea
Whitehat1241 <89269388+Whitehat1241@users.noreply.github.com>
2021-11-08 12:07:33 -0500

    Update MAIN- CALC.py

--- MAIN- CALC.py
+++ MAIN- CALC.py
@@ -1,114 +1,125 @@
 # Schneider Theodule
-# Program will calculate basic math function (Integration 1)
-# This program will also take the square root and perform quadratic equations (Integration 2)
 
+# Integration 1: Program will calculate basic math function
+# Integration 2: This program will also take the square root and perform quadratic equations
 
 import math
 
 # Loop to preform multiple operations at once
-a = 'SELECT'
-b = 'OPERATION'
-greeting = [a, b]
-print(*greeting, sep=" AN ", end="\n    -------\n", )
-while True:
+start = print(input("To begin, select any key followed by "
+                    "the enter button: ",))
+beginList = {start}
 
-    # Define Operations
-    print("q or Q: QUIT")
-    print("1: Addition")
-    print("2: Subtraction")
-    print("3: Multiplication")
-    print("4: Division")
-    print("5: Raise to a Power")
-    print("6: Square Root")
-    print("7: Quadratic formula", end="\n     -------\n")
-    options = ['1', '2', '3', '4', '5', '6', '7', 'q', 'Q']
 
-    choice = input("Enter Your Choice: ", )
+for start in beginList:
 
-    if choice in options:
-        try:
-            # Check for which operation is selected & what to do
-            if int(choice) == 1:  # Addition
-                num1 = float(input("Enter Number 1: ", ))
-                num2 = float(input("Enter Number 2: ", ))
-                print(num1, "+", num2, "=", (num1 + num2))
+    a = 'SELECT'
+    b = 'OPERATION'
+    greeting = [a, b]
+    print(*greeting, sep=" AN ", end="\n    -------\n", )
+    while True:
 
-            elif int(choice) == 2:  # Subtraction
-                num1 = float(input("Enter Number 1: ", ))
-                num2 = float(input("Enter Number 2: ", ))
-                print(num1, "-", num2, "=", (num1 - num2))
+        # Define Operations
+        print("q or Q: QUIT")
+        print("1: Addition")
+        print("2: Subtraction")
+        print("3: Multiplication")
+        print("4: Division")
+        print("5: Raise to a Power")
+        print("6: Square Root")
+        print("7: Quadratic formula", end="\n     -------\n")
+        options = ['1', '2', '3', '4', '5', '6', '7', 'q', 'Q']
 
-            elif int(choice) == 3:  # Multiplication
-                num1 = float(input("Enter Number 1: ", ))
-                num2 = float(input("Enter Number 2: ", ))
-                print(num1, "*", num2, "=", (num1 * num2))
+        choice = input("Enter Your Choice: ", )
 
-            elif int(choice) == 4:  # Division
-                try:
+        if choice in options:
+            try:
+                # Check for which operation is selected & what to do
+                if int(choice) == 1:  # Addition
                     num1 = float(input("Enter Number 1: ", ))
                     num2 = float(input("Enter Number 2: ", ))
-                    if num2 == 0.0:  # Checking if user selects 0.0
-                        print("ERROR: DIVIDE BY 0")
-                    else:
-                        print("\nResult: {} / {} = {:.2f}".format(num1, num2, num1 / num2))
-                        print("\nRemainder :", (num1 % num2))
-                except:
-                    print("ERROR")
+                    print(num1, "+", num2, "=", (num1 + num2))
 
-            elif int(choice) == 5:  # Raising to a Power
-                num1 = float(input("Enter Number 1: ", ))
-                num2 = float(input("Enter Number 2: ", ))
-                print(num1, "**", num2, "=", (num1 ** num2))
+                elif int(choice) == 2:  # Subtraction
+                    num1 = float(input("Enter Number 1: ", ))
+                    num2 = float(input("Enter Number 2: ", ))
+                    print(num1, "-", num2, "=", (num1 - num2))
 
-            elif int(choice) == 6:  # Square Root
-                while True:
+                elif int(choice) == 3:  # Multiplication
+                    num1 = float(input("Enter Number 1: ", ))
+                    num2 = float(input("Enter Number 2: ", ))
+                    print(num1, "*", num2, "=", (num1 * num2))
+
+                elif int(choice) == 4:  # Division
                     try:
-                        num1 = float(input("Enter Power of Root: ", ))
-                        num2 = float(input("Enter the Radicand:", ))
-                        print(int(num1), "√", int(num2), " = ", num2 ** (1 / num1))
-                        break
+                        num1 = float(input("Enter Number 1: ", ))
+                        num2 = float(input("Enter Number 2: ", ))
+                        if num2 == 0.0:  # Checking if user selects 0.0
+                            print("ERROR: DIVIDE BY 0")
+                        else:
+                            print("\nResult: {} / {} = {:.2f}".format(num1, num2, num1 / num2))
+                            print("\nRemainder :", (num1 % num2))
                     except:
-                        pass
+                        print("ERROR")
 
-            elif int(choice) == 7:  # Quadratic Formula
-                possible = True
-                while possible:
+                elif int(choice) == 5:  # Raising to a Power
+                    num1 = float(input("Enter Number 1: ", ))
+                    num2 = float(input("Enter Number 2: ", ))
+                    print(num1, "**", num2, "=", (num1 ** num2))
 
-                    a = float(input("a=? ", ))
-                    b = float(input("b=? ", ))
-                    c = float(input("c=? ", ))
-                    d = (b ** 2)
-                    e = (4 * a * c)  # For final project, delete variables d & e// change variable f to d----|
-                    f = d - e
-                    if (d != 0) and (d > e):
-                        if (f > 0) or (f == 0):  # This part will return a value if (b**2) > (4 * a * c) or else it will
-                                                # be an imaginary value
-
-                            solution1 = (-b + math.sqrt(f)) / (2 * a)
-                            solution2 = (-b - math.sqrt(f)) / (2 * a)
-                            answer = "ROOTS EQUAL: {:.10f} and {:.10f}".format(solution1, solution2)
-                            print("Result: ", answer)  # I want answer to have 10 numbers// 10 after decimal and 10
-                                                                # with decimal
-
-                            print("Done!", end="\n     -------\n")
-                            possible = not True
-                        elif (b ** 2) < (4 * a * c):
-                            print("ERROR: NON-REAL ANSWER", "\n    XXXXXXX    ")
+                elif int(choice) == 6:  # Square Root
+                    while True:
+                        try:
+                            num1 = float(input("Enter Power of Root: ", ))
+                            num2 = float(input("Enter the Radicand:", ))
+                            print(int(num1), "√", int(num2), " = ", num2 ** (1 / num1))
                             break
-                    else:
-                        pass
-        except:
-            if str(choice) == 'q' or str(choice) == 'Q':
-                print("Done!")
-                break
-            else:
-                print("Invalid")
-    else:
-        print("   XXXX-XXXX-XXXX-XXXX  ", "\nINVALID INPUT: TRY AGAIN!", )
-        print("     -------")
+                        except:
+                            pass
+
+                elif int(choice) == 7:  # Quadratic Formula
+                    possible = True
+                    while possible:
+
+                        a = float(input("a=? ", ))
+                        b = float(input("b=? ", ))
+                        c = float(input("c=? ", ))
+                        d = (b ** 2)
+                        e = (4 * a * c)  # For final project, delete variables d & e// change variable f to d----|
+                        f = d - e
+                        if (d != 0) and (d > e):
+                            if (f > 0) or (f == 0):  # This part will return a value if (b**2) > (4 * a * c) or else it
+                                # will be an imaginary value
+
+                                solution1 = (-b + math.sqrt(f)) / (2 * a)
+                                solution2 = (-b - math.sqrt(f)) / (2 * a)
+                                answer = "ROOTS EQUAL: {:.10f} and {:.10f}".format(solution1, solution2)
+                                print("Result: ", answer)  # I want answer to have 10 numbers// 10 after decimal and 10
+                                # with decimal
+
+                                print("Done!", end="\n     -------\n")
+                                possible = not True
+                            elif (b ** 2) < (4 * a * c):
+                                print("ERROR: NON-REAL ANSWER", "\n    XXXXXXX    ")
+                                break
+                        else:
+                            break
+            except:
+                if str(choice) != 'q' and str(choice) != 'Q':
+                    print("Invalid")
+                else:
+                    print("Done!")
+                    break
+        else:
+            print("   XXXX-XXXX-XXXX-XXXX  ", "\nINVALID INPUT: TRY AGAIN!", )
+            print("     -------")
 
 
+# Operating system
 # ****import parser? what is that
+
+#**** An operating system is software/program that acts as an interface between the user and the computer hardware
+# controls the execution of all kinds of programs*****
 
 
 # Got help from:
@@ -126,3 +137,4 @@ while True:
         # quadratic formula: https://www.youtube.com/watch?v=qXW-9jYYja8
 
         # Jakeb Brown: while loops, formatting, algorithmic fixes
+
